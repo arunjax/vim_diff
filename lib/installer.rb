9118695ede56d249cc484dae40976f0746397a28
Arun Kumar <arun@chronus.com>
2011-06-02 10:06:32 +0530

    Fixing typo in the Installer Class

--- lib/installer.rb
+++ lib/installer.rb
@@ -1,6 +1,6 @@
 module VimDiff
   class Installer
-    def isntall
+    def install
       vim_plugin_path = "~/.vim/plugin"
       puts `mkdir -p #{vim_plugin_path}`
       puts `cp #{File.join(File.dirname(__FILE__), '..', 'vim_plugin', 'tohtml.vim')} #{vim_plugin_path}`
